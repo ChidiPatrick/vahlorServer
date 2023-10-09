7bc65f8510125054b483458a5d752e28f82c62f1
Patrick Chidiebele Okafor <okaforpatrick302@gmail.com>
2023-10-09 19:06:31 +0100

    Implemented session management using cookie-session middleware

--- routes/user auth/signin.with.google.js
+++ routes/user auth/signin.with.google.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const passport = require("passport");
-const session = require("express-session");
+const cookieSession = require("cookie-session");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require("dotenv").config();
 const cors = require("cors");
@@ -11,6 +11,8 @@ const signInWithGoogle = express.Router();
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.SECRET_KEY,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -26,8 +28,24 @@ const verifyCallback = (accessToke, refreshToken, profile, done) => {
 
 passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallback));
 
-signInWithGoogle.use(passport.initialize());
+signInWithGoogle.use(
+  cookieSession({
+    name: "Session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((userObject, done) => {
+  done(null, userObject);
+});
+
+signInWithGoogle.use(passport.initialize());
+signInWithGoogle.use(passport.session());
 signInWithGoogle.use(
   "/",
   passport.authenticate("google", {
@@ -43,7 +61,7 @@ signInWithGoogle.get(
   passport.authenticate("google", {
     failureRedirect: "/failure",
     successRedirect: "/secretPage",
-    session: false,
+    session: true,
   }),
   (req, res) => {
     console.log("Google called us back");
